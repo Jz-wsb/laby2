784734806064b4703190991fe6f645b0be7a1b84
Jz-wsb <wrx81999@student.wroclaw.merito.pl>
2024-06-03 17:44:18 +0200

    setting up CI with GitHub Actions and adding Flask test

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from ... import app 
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    rv = client.get('/')
+    assert rv.data == b"Hello, World!"
+    assert rv.status_code == 200
